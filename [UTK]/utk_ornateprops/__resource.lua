b8fbe1b960d7863f0ef22f7e62352026d7e7b014
utkuali <57073765+utkuali@users.noreply.github.com>
2020-02-08 13:57:39 +0300

    update manifest

--- [UTK]/utk_ornateprops/__resource.lua
+++
@@ -1,6 +0,0 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
-files {
-}
-
-data_file 'DLC_ITYP_REQUEST' 'coming soon'
---
+++ [UTK]/utk_ornateprops/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+
+game "gta5"
+
+files {
+}
+
+data_file 'DLC_ITYP_REQUEST' 'coming soon'
