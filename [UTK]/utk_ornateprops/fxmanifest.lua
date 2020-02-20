3a9e239cbe0534554b3b0624ac52cf36b04e21a6
utkuali <57073765+utkuali@users.noreply.github.com>
2020-02-20 18:22:55 +0300

    Add files via upload

--- [UTK]/utk_ornateprops/fxmanifest.lua
+++ [UTK]/utk_ornateprops/fxmanifest.lua
@@ -1,8 +1,14 @@
-fx_version 'adamant'
-
+fx_version "adamant"
 game "gta5"
-
 files {
+    "stream/ch_prop_gold_bar_01a.ydr",
+    "stream/ch_prop_gold_trolly_01a.ydr",
+    "stream/ch_prop_gold_trolly_01a.ytd",
+    "stream/ch_prop_gold_trolly_01b.ydr",
+    "stream/ch_prop_gold_trolly_01b.ytd",
+    "stream/ch_prop_gold_trolly_01c.ydr",
+    "stream/ch_prop_gold_trolly_01c.ytd",
+    "stream/ch_prop_gold_trolly_empty.ydr",
+    "stream/ch_prop_gold_trolly_empty.ytd"
 }
-
-data_file 'DLC_ITYP_REQUEST' 'coming soon'
+data_file "DLC_ITYP_REQUEST" "stream/props.ytyp"
