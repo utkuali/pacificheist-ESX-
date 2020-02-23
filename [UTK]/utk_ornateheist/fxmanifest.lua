7e90b1698acd7cd372321c6d973faa36154ba9d0
Yagiz ALP <yagizwasmeow@gmail.com>
2020-02-23 18:56:39 +0300

    Cleanup for fxmanifest
    Cleanup :)

--- [UTK]/utk_ornateheist/fxmanifest.lua
+++ [UTK]/utk_ornateheist/fxmanifest.lua
@@ -1,9 +1,14 @@
-fx_version "adamant"
-game "gta5"
-client_script "client.lua"
-server_script "server.lua"
+fx_version 'adamant'
+
+game 'gta5'
+
+client_script 'client.lua'
+-- client_script 'clientpNotify.lua'
+
+server_script 'server.lua'
 files {
     "html/alarm.html",
     "html/alarm.ogg"
 }
-ui_page "html/alarm.html"
+
+ui_page 'html/alarm.html'
