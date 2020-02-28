718dfad9a81f6df58a830b4cd7fac3b2f14a8abf
utkuali <57073765+utkuali@users.noreply.github.com>
2020-02-28 21:45:55 +0300

    Update fxmanifest.lua

--- [UTK]/utk_ornateheist/fxmanifest.lua
+++ [UTK]/utk_ornateheist/fxmanifest.lua
@@ -3,9 +3,9 @@ fx_version 'adamant'
 game 'gta5'
 
 client_script 'client.lua'
--- client_script 'clientpNotify.lua'
 
 server_script 'server.lua'
+
 files {
     "html/alarm.html",
     "html/alarm.ogg"
