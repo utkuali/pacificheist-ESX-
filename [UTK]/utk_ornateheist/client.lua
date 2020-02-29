f59dc2703cee8666165537c131ad437af69ac1ab
NotPixel <61543409+NotPixel@users.noreply.github.com>
2020-02-29 15:23:31 +0000

    Fixed 3D text now showing on start.

--- [UTK]/utk_ornateheist/client.lua
+++ [UTK]/utk_ornateheist/client.lua
@@ -1,6 +1,20 @@
 -- You have issues? Use Github
 
 ESX = nil
+
+Citizen.CreateThread(function()
+  	while ESX == nil do
+    	TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    	Citizen.Wait(250)
+  	end
+
+  	while ESX.GetPlayerData().job == nil do
+		Citizen.Wait(250)
+	end
+	
+	ESX.PlayerData = ESX.GetPlayerData()
+end)
+
 PoliceDoors = {}
 PlayerData = nil
 UTK = {
