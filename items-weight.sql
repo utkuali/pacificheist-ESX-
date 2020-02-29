ccfab69fe5deefb0456d4771bdca41db181e064c
NotPixel <61543409+NotPixel@users.noreply.github.com>
2020-02-29 14:22:33 +0000

    Create items-weight.sql

---
+++ items-weight.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO `items` (`name`, `label`) VALUES 
+    ('id_card', 'ID Card'),
+    ('thermal_charge', 'Thermal Charge'),
+    ('laptop_h', 'Hacker Laptop'),
+    ('lockpick', 'Lockpick'),
+    ('gold_bar', 'Gold Bar),
+    ('dia_box', 'Diamond Box);
+    
+-- This version is for weight-based SQL.
